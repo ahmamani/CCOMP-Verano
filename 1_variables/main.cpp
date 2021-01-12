1972a06c97b7cdb22ae8630852f2a5fd2ee6731d
Alvaro Mamani Aliaga <amamaniali@unsa.edu.pe>
2021-01-12 14:35:27 -0500

    Ejemplo de variable con tipo INT

---
+++ 1_variables/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Declaración e inicialización en una sola linea.
+    int a = 10;
+
+    // Pero, puedo declarar y despues inicializar.
+    int b;
+
+    b = 20;
+
+    cout << a << endl;
+    cout << b << endl; 
+
+}
